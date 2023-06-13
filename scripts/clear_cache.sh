a3885b237acdb03682a3e151b9cd97f86022008c
Melanie Kuhles <melanie.kuhles@oder-spree-consulting.de>
2023-06-13 12:21:34 +0200

    add clear and rebuild script

--- scripts/clear_cache.sh
+++ scripts/clear_cache.sh
@@ -2,4 +2,4 @@ if [ -f ../.env ]; then
   source ../.env;
 fi
 
-docker exec -w /opt/drupal -it $DRUPAL_CONTAINER_NAME  bash -c "drush cache:clear"
+docker exec -w /opt/drupal -it $DRUPAL_CONTAINER_NAME  bash -c "drush cr"
