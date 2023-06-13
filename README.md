2b70133f625a8e7f15bc48b338df06561e91d90b
Melanie Kuhles <melanie.kuhles@oder-spree-consulting.de>
2023-06-13 11:25:00 +0200

    more explanations

--- README.md
+++ README.md
@@ -5,4 +5,9 @@ This Project sets up an docker environment for module development.
 Before you start change `MODULES_NAME` in `.env`.
 Afterwards run `./startover.sh` from within `scripts/` folder.
 
-While setup you'll be asked the drush setup questions, besides the module name. 
+While setup you'll be asked the drush setup questions, besides the module name.
+
+Afterwards you have to change the owner from the generated files on your host system. After generation it is root:root.
+
+- Call `composer init` for creating a composer.json file. See: [Add a composer.json file](https://www.drupal.org/docs/develop/creating-modules/add-a-composerjson-file)
+- for more information about writing a drupal module see [Creating modules](https://www.drupal.org/docs/develop/creating-modules)
--- scripts/init.sh
+++ scripts/init.sh
@@ -6,14 +6,19 @@ fi
 DB_URL="mysql://$MARIADB_USER:$MARIADB_PASSWORD@$DATABASE_CONTAINER_NAME:$DATABASE_PORT/$MARIADB_DATABASE"
 sleep 5
 
-docker exec $DRUPAL_CONTAINER_NAME pwd
 docker exec -w /opt/drupal $DRUPAL_CONTAINER_NAME bash -c "composer require drush/drush ; composer update"
 
 echo '' 
-echo 'install drupal on container'
+echo -e "Install drupal on the container:\n"
 
 docker exec -w /opt/drupal $DRUPAL_CONTAINER_NAME drush si --db-url=$DB_URL --account-pass=$DRUPAL_ADMIN_PASS --site-name="$SITE_NAME" -y
 
 docker exec $DRUPAL_CONTAINER_NAME chown www-data:www-data /opt/drupal/web/sites/default/files -R
 
 docker exec -it $DRUPAL_CONTAINER_NAME  bash -c "drush generate module --answer=$MODULES_NAME --answer=$MODULES_NAME"
+
+echo 'You have to change the owner from the generated files on your host system. By now it is root.'
+echo "> sudo chown $USER:$(id -gn) $MODULES_NAME.info.yml"
+echo "> sudo chown $USER:$(id -gn) $MODULES_NAME.install"
+echo -e "> sudo chown $USER:$(id -gn) $MODULES_NAME.module\n"
+echo -e "Call 'composer init' for creating a composer.json file.\n"
